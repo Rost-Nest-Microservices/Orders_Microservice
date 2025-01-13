eaf15dd8d4aacc813796c8e3ff1efa87e0371a56
jpena <juan.pena@wmcontractor.com>
2025-01-12 23:11:37 -0500

    First strcuture for orders-microservice

---
+++ src/config/envs.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const { error, value: envVars } = envsSchema.validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error ${error.message}`);
+}
+
+export const envs = {
+  port: envVars.PORT as number,
+};
